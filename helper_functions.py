f69e60ec5e690445814d8c3646a615ab24f00f2d
visaals <visaal.sivakumar@gmail.com>
2018-07-21 18:00:09 -0500

    moved helper

---
+++ helper_functions.py
@@ -0,0 +1,79 @@
+"""
+Input: One 2d array
+Output: 4 2d arrays
+"""
+def split2DArrayIntoFour(arr):
+
+    if type(arr) is not list:
+        return "Error: input is not a list"
+    length = len(arr)
+    if length is 0:
+        return []
+    if type(arr[0]) is not list:
+        return "Error: input is a 1d array not a 2d array"
+    width = len(arr[0])
+
+    half_length = int(length/2)
+    half_width = int(width/2)
+
+    splicedArray = []
+
+    # top left array
+    splice = []
+    for i in range(0, half_length):
+        temp = []
+        for j in range(0, half_width):
+            temp.append(arr[i][j])
+        splice.append(temp)
+    splicedArray.append(splice)
+
+    # top right array
+    splice = []
+    for i in range(half_length, length):
+        temp = []
+        for j in range(0, half_width):
+            temp.append(arr[i][j])
+        splice.append(temp)
+    splicedArray.append(splice)
+
+    # bottom left array
+    splice = []
+    for i in range(0, half_length):
+        temp = []
+        for j in range(half_width, width):
+            temp.append(arr[i][j])
+        splice.append(temp)
+    splicedArray.append(splice)
+
+    # bottom right array
+    splice = []
+    for i in range(half_length, length):
+        temp = []
+        for j in range(half_width, width):
+            temp.append(arr[i][j])
+        splice.append(temp)
+    splicedArray.append(splice)
+
+
+    return splicedArray
+
+
+arr = [[1,2,3,4],
+       [5,6,7,8],
+       [9,10,11,12],
+       [13,14,15,16]]
+
+arr1 = [[1,2,3,4,100],
+       [5,6,7,8,101],
+       [9,10,11,12,102],
+       [13,14,15,16,103]]
+
+arr2 = [[1,2,3,4,100],
+       [5,6,7,8,101],
+       [9,10,11,12,102],
+       [13,14,15,16,103],
+       [17,18,19,20,104]]
+
+x = split2DArrayIntoFour(arr2)
+for i in x:
+    print(i)
