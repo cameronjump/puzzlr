7346aa421ee60da1d8ea9988d5ea3db7bf51e5b2
cameronjump <cameron.jump@okstate.edu>
2018-07-21 23:44:27 -0500

    some gud stuff

--- android/app/src/main/java/room323/puzzlr/Frisbee.java
+++ android/app/src/main/java/room323/puzzlr/Frisbee.java
@@ -4,18 +4,80 @@ import android.util.Log;
 
 import com.cloudinary.android.MediaManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Frisbee {
 
-    private static void uploadFile(String path) {
+    private static final String TAG = "FRISBEE";
+
+    public static String uploadFile(String path) {
 
         File file = new File(path);
         if (file.exists()) {
             String requestId = MediaManager.get().upload(path).dispatch();
-            Log.d("Frisbee", requestId);
+            Log.d(TAG, requestId);
+            return requestId;
         } else {
-            Log.d("Frisbee", path + "not found");
+            Log.d(TAG, path + "not found");
+            return "File not found";
         }
     }
+
+    public static void uploadAndNotifyFlask(String path, int pieces) {
+        String id = uploadFile(path);
+        try {
+            JSONObject json = new JSONObject();
+            json.put("id", id);
+            json.put("pieces", pieces);
+            notifyFlask(json);
+        }
+        catch (JSONException e) {
+            ;
+        }
+    }
+
+    public static void notifyFlask(final JSONObject json) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https://dry-falls-41246.herokuapp.com/process_image");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept", "application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(json.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.d(TAG, String.valueOf(conn.getResponseCode()));
+                    Log.d(TAG, conn.getResponseMessage());
+                    InputStream is = conn.getInputStream();
+                    Scanner s = new Scanner(is).useDelimiter("\\A");
+                    String result = s.hasNext() ? s.next() : "";
+                    Log.d(TAG, result);
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    Log.d(TAG, e.toString());
+                }
+            }
+        });
+        thread.start();
+    }
+
 }
--- android/app/src/main/java/room323/puzzlr/PieceMatchActivity.java
+++ android/app/src/main/java/room323/puzzlr/PieceMatchActivity.java
@@ -5,32 +5,27 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.cloudinary.android.MediaManager;
 
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
 
 public class PieceMatchActivity extends AppCompatActivity {
 
     private static final String TAG = "PieceMatchActivity";
     private ImageView imageview;
+
+    private String imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,102 +51,53 @@ public class PieceMatchActivity extends AppCompatActivity {
                 cameraDialog();
             }
         });
-    }
 
-        /*try {
-            cloudinary.uploader().upload(path, new HashMap());
-        }
-        catch (Exception e) {
-            Log.d("Pizza", e.toString());
-        }*/
-
-
-
-
-
-
-
-        /*try {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        URL url = new URL("https://dry-falls-41246.herokuapp.com/process_image");
-                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                        conn.setRequestMethod("POST");
-                        conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                        conn.setRequestProperty("Accept","application/json");
-                        conn.setDoOutput(true);
-                        conn.setDoInput(true);
-
-                        JSONObject jsonParam = new JSONObject();
-
-                        jsonParam.put("image", bitMapToString(bitmap));
-
-                        Log.d(TAG, jsonParam.toString());
-                        DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                        //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-                        os.writeBytes(jsonParam.toString());
-
-                        os.flush();
-                        os.close();
-
-                        Log.d(TAG, String.valueOf(conn.getResponseCode()));
-                        Log.d(TAG , conn.getResponseMessage());
-                        InputStream is = conn.getInputStream();
-                        Scanner s = new Scanner(is).useDelimiter("\\A");
-                        String result = s.hasNext() ? s.next() : "";
-                        Log.d(TAG, result);
-
-                        conn.disconnect();
-                    } catch (Exception e) {
-                        Log.d(TAG, e.toString());
-                    }
-                }
-            });
-
-            thread.start();
-        }
-        catch (Exception e) {
-            Log.d(TAG, e.toString());
-        }*/
-
-
-    private byte[] getBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
-
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
-        }
-        return byteBuffer.toByteArray();
+        Button uploadButton = findViewById(R.id.upload);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upload();
+            }
+        });
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         if(resultCode == RESULT_OK){
-            Bitmap imageBitmap;
             if (requestCode == 0) {
                 Bundle extras = imageReturnedIntent.getExtras();
-                imageBitmap = (Bitmap) extras.get("data");
-                imageview.setImageBitmap(imageBitmap);
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Uri uri = MyUtils.getImageUri(this, imageBitmap);
+                imageview.setImageURI(uri);
+                imagePath = MyUtils.getRealPathFromURI(this, uri);
             }
             else if (requestCode == 1) {
                 Uri selectedImage = imageReturnedIntent.getData();
                 imageview.setImageURI(selectedImage);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                imageBitmap = BitmapFactory.decodeFile(getRealPathFromURI(selectedImage), options);
-                imageview.setImageBitmap(imageBitmap);
-                Log.d("Frisbee", getRealPathFromURI(selectedImage));
-                Frisbee.uploadFile(getRealPathFromURI(selectedImage));
+                imagePath = MyUtils.getRealPathFromURI(this, selectedImage);
             }
         }
     }
 
-    private void cameraDialog() {
+    private void upload() {
+        EditText numberInput = findViewById(R.id.number);
+        String number = numberInput.getText().toString();
+        Log.d(TAG, number);
+        if (number.equals("") && imagePath == null) {
+            Toast.makeText(this, "Please upload image and enter number of pieces.", Toast.LENGTH_SHORT).show();
+        }
+        else if(number.equals("")) {
+            Toast.makeText(this, "Please upload image.", Toast.LENGTH_SHORT).show();
+        }
+        else if(imagePath == null) {
+            Toast.makeText(this, "Please enter number of pieces.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Frisbee.uploadAndNotifyFlask(imagePath, Integer.parseInt(number));
+        }
+    }
+
+    public void cameraDialog() {
         AlertDialog alertDialog = new AlertDialog.Builder(PieceMatchActivity.this).create();
         alertDialog.setTitle("Image Upload");
         alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Camera",
@@ -179,22 +125,6 @@ public class PieceMatchActivity extends AppCompatActivity {
                 });
         alertDialog.show();
     }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-    public String bitMapToString(Bitmap bitmap){
-        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-        byte [] b=baos.toByteArray();
-        String temp=Base64.encodeToString(b, Base64.DEFAULT);
-        return temp;
-    }
-
 
 }
 
